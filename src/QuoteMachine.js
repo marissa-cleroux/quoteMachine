4f2dc5f0713a6b470bf6ee7f241c6cf9062259d6
Marissa Cleroux <marissa.cleroux@gmail.com>
2019-07-11 20:02:30 -0400

    Generates new quote, updates to UI

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -4,41 +4,51 @@ import "./App.css";
 function Header() {
   return (
     <header>
-      <h1>Quotes from Sci-Fi Ladies</h1>
+      <h1>Sci-Fi Ladies</h1>
     </header>
   );
 }
 function Quote(props) {
-  return <div id="text">{props.Quote}</div>;
+  return <div id="text">{props.quote}</div>;
 }
 
 function Author(props) {
-  return <div id="author">{props.Author}</div>;
+  return <div id="author">{props.author}</div>;
 }
 
 function Actions(props) {
   return (
     <div className="action">
-      <button id="new-quote" onClick={props.newQuote} className="button">
-        New Quote
-      </button>
-
-      <a href="twitter.com/intent/tweet" id="tweet-quote" className="button">
-        TWEET
-        <img src="./images/twitter.png" alt="twitter icon" className="icon" />
+      <a
+        href="https://twitter.com/intent/tweet"
+        id="tweet-quote"
+        className="button"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="./images/twitter.png" alt="twitter symbol" className="icon" />
       </a>
+      <button
+        id="new-quote"
+        onClick={() => {
+          props.newQuote();
+        }}
+        className="button"
+      >
+        <img src="./images/next.png" alt="next quote" className="icon" />
+      </button>
     </div>
   );
 }
 function QuoteMachine(props) {
   return (
-    <div id="quote-box">
+    <div>
       <Header />
-      <article id="quote-box">
-        <Quote Quote={props.Quote} />
-        <Author Author={props.Author} />
-        <Actions />
-      </article>
+      <main id="quote-box">
+        <Quote quote={props.quote} />
+        <Author author={props.author} />
+        <Actions newQuote={props.newQuote} />
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,25 +9,30 @@ let state = {};
 function getRandomQuote() {
   var proxyUrl = "https://cors-anywhere.herokuapp.com/",
     targetUrl = "https://sci-fi-ladies-api.herokuapp.com/api/quote";
+
+  console.log("CALLED GET RANDOM QUOTE");
+
   fetch(proxyUrl + targetUrl)
     .then(blob => blob.json())
     .then(data => {
-      state["Author"] = data[0].author;
-      state["Quote"] = data[0].quote;
+      state.Author = data[0].author;
+      state.Quote = data[0].quote;
       console.log("DATA: ", data);
       console.log("STATE: ", state);
-
       render();
-      return data;
     })
     .catch(e => {
-      return e;
+      console.log("ERROR: ", e);
     });
 }
 
 function render() {
   ReactDOM.render(
-    <QuoteMachine Author={state.Author} Quote={state.Quote} />,
+    <QuoteMachine
+      author={state.Author}
+      quote={state.Quote}
+      newQuote={getRandomQuote}
+    />,
     document.getElementById("root")
   );
 }
